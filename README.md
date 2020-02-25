e735c390df84696d8dfb1e04441978998ac14246
CoreyDeJong <dejongcorey@gmail.com>
2020-02-25 11:22:37 -0800

    completed card 2

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # book_app
 
-Number and name of feature: lab 11 - EJS
+Number and name of feature: 1 - home page render
 
 Estimate of time needed to complete: __2hrs___
 
 Start time: 9am_____
 
-Finish time: _____
+Finish time: _11:08____
 
-Actual time needed to complete: _____
+Actual time needed to complete: __2.1hours___
+
+
+
+
+Number and name of feature: 2 - create user form, send to api
+
+Estimate of time needed to complete: 1hr
+
+Start time: _11:10
+
+Finish time: 12:10
+
+Actual time needed to complete: 
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 require('dotenv').config();
 const express = require('express');
@@ -7,16 +7,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 require('ejs');
-app.use(express.static('./public/styles'));
+app.use(express.static('./public/'));
 
 //tells the server(express) to use the ejs template view engine
 app.set('view engine', 'ejs')
 
 //putting the ejs file into view on the front end
 app.get('/', (req, res) =>{
-    res.render('index.ejs');
+    res.render('./pages/index.ejs');
 })
 
+app.get('/searches/new', (req, res)=>{
+    res.render('./pages/searches/show.ejs');
+})
 
 
 
